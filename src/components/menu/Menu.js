65823f118ba4fb2968f6f3bb70ed8aba8e497d24
gabriele <gabriieele6@gmail.com>
2020-05-08 19:04:06 -0300

    Adicionando o Menu e alterando o template

---
+++ src/components/menu/Menu.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import "./menu.css";
+
+export default class Menu extends Component {
+  render() {
+    return (
+      <nav class="navbar navbar-expand-lg navbar-dark teste">
+        <a class="navbar-brand" href="#">
+          Girls'N Code
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarText"
+          aria-controls="navbarText"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Home
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Sobre
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
